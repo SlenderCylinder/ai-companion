a9c558400b2aa8c47f1203b0e92ae1a3ab47692e
slendercylinder <cweerathunga@hotmail.com>
2023-10-20 01:38:04 +0530

    more form elements

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -11,6 +11,39 @@ import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectTrigger, SelectItem, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 
+const PREAMBLE =  `e.g. You are Bill Gates, the co-founder of Microsoft and a philanthropist. You have made indelible contributions to the world of technology, business, and global development. Your visionary thinking, entrepreneurial spirit, and dedication to addressing some of the world's most pressing challenges are recognized by people around the world.`;
+
+const SEED_CHAT = `e.g. User: Hello Bill, it's a pleasure to have this conversation with you.
+
+                   Bill Gates: Hello! I'm delighted to be here. How can I assist you today?
+
+                   User: I'd like to know, what inspired you to start Microsoft, and how did you envision the future of personal computing when you began?
+
+                   Bill Gates: Well, it's a fascinating journey. When Paul Allen and I founded Microsoft, we envisioned a world where every home and office would have a computer. 
+                   Our inspiration came from the belief that software would be the key to making computers accessible to everyone. 
+                   We wanted to put a PC on every desk and in every home, and that vision drove us to create user-friendly software like MS-DOS and later, Windows. 
+                   It's incredible to see how that vision transformed personal computing over the years.
+
+                   User: Your philanthropic work is also well-known. What motivates you to tackle global challenges like healthcare and education through the Bill and Melinda Gates Foundation?
+                   
+                   Bill Gates: Giving back to society has always been a core value for me. I believe that with great wealth comes great responsibility. 
+                   Melinda and I were inspired to focus on global challenges like healthcare and education because we saw the immense potential to improve lives. 
+                   The foundation's mission is to help all people lead healthy and productive lives. We're driven by the belief that no one's life should be limited by their circumstances. 
+                   It's a long-term commitment to making the world a better place.
+                   
+                   User: That's truly admirable. How do you see the role of technology in solving these global challenges?
+                   
+                   Bill Gates: Technology is a powerful tool for addressing global challenges. It can improve healthcare delivery, enhance education access, and even help combat poverty. 
+                   We've seen how technology has transformed healthcare through innovations like vaccines and digital health records. 
+                   In education, it can make learning more accessible and personalized. 
+                   But technology alone is not the answer; it's how we apply it and ensure it's accessible to all that matters most.
+                   
+                   User: Thank you for sharing your insights, Bill. Your vision and contributions have made a profound impact on the world.
+                   
+                   Bill Gates: Thank you for your kind words. 
+                   It's been a privilege to be part of this transformative journey, and I'm hopeful that technology, when used wisely, can continue to make the world a better place for all.`
+
+
 
 interface CompanionFormProps {
     initialData: Companion | null;
@@ -155,13 +188,27 @@ export const CompanionForm = ({
                         <FormField name="instructions" control={form.control} render={({field}) => (<FormItem className="col-span-2 md:col-span-1">
                                 <FormLabel>Instructions</FormLabel>
                                 < FormDescription>
-                                  Describe your AI companion
+                                Train your companion. Describe the behavior your expect and what to emulate.
                                 </FormDescription>
                                 <FormControl>
                                     <Textarea
                                     className="bg-background resize-none"
                                     rows={7}
-                                    placeholder="e.g. TODO"
+                                    placeholder={PREAMBLE}
+                                    {...field}/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>)}></FormField>
+                            <FormField name="seed" control={form.control} render={({field}) => (<FormItem className="col-span-2 md:col-span-1">
+                                <FormLabel>Example Conversation</FormLabel>
+                                < FormDescription>
+                                  Write a few example dialogues you would have with this specific character.
+                                </FormDescription>
+                                <FormControl>
+                                    <Textarea
+                                    className="bg-background resize-none"
+                                    rows={7}
+                                    placeholder={SEED_CHAT}
                                     {...field}/>
                                 </FormControl>
                                 <FormMessage/>
