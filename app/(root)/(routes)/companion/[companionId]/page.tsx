312eccbffffd8cf33772f8c7a784b5abf99fe48c
slendercylinder <cweerathunga@hotmail.com>
2023-12-25 23:17:51 +0530

    comment

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -34,6 +34,7 @@ const RootPage = async ({
     })
 
     const categories = await prismadb.category.findMany();
+    //fix this  
     return (
         <div className="h-full p-4 space-y-2">
            <SearchInput/>
