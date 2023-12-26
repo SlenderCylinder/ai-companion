4c5476f12ecbf9ee7dd964ee803535815833c996
slendercylinder <cweerathunga@hotmail.com>
2023-12-26 10:46:11 +0530

    chat landing page

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,4 +1,41 @@
-const ChatIdPage = () => {
+import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/dist/server/api-utils";
+
+interface ChatIdPageProps {
+    params: {
+        chatId: string;
+    }
+}
+
+
+const ChatIdPage = async ({params}: ChatIdPageProps) => {
+    const {userId} = auth();
+
+    if (!userId) {
+        return redirectToSignIn();
+    }
+
+    const companion = await prismadb.companion.findUnique({
+        where: {
+            id: params.chatId
+        },
+        include: {
+            messages: {
+                orderBy: {
+                    createdAt: "asc",
+                },
+                where: {
+                    userId,
+                }
+            },
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
     return (
         <div>
             Hello Chat!
