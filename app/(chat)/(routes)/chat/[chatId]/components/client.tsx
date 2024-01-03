c4a232b33db50015d5a1a40f317517751f2609a4
slendercylinder <cweerathunga@hotmail.com>
2024-01-03 10:30:57 +0530

    Minor error fix

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -2,7 +2,7 @@
 
 import { Companion, Message} from "@prisma/client"
 import { ChatHeader } from "@/components/chat-header"
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useCompletion } from "ai/react"
 
