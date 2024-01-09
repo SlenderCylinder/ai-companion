60782e5a8e68964d4b8a0cd138905e75fbc7cad3
slendercylinder <cweerathunga@hotmail.com>
2024-01-09 11:18:22 +0530

    messages skeleton

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Companion } from "@prisma/client";
+import { ChatMessage } from "./chat-message";
 
 interface ChatMessagesProps {
     messages: any[];
@@ -10,9 +11,14 @@ interface ChatMessagesProps {
 }
 
 export const ChatMessages = ({messages = [], isLoading, companion}: ChatMessagesProps) => {
+
     return(
-        <div>
-            Chat messages go here!.
+        <div className="flex-1 overflow-y-auto pr-4">
+            <ChatMessage 
+                src={companion.src}
+                role="system"
+                content={`Hello! I am ${companion.name}, ${companion.description}`}
+            />
         </div>
     )
 
