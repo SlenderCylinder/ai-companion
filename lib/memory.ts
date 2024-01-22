5dd50c3afb1f52b7d2988e69b8ad00aadc56559c
slendercylinder <cweerathunga@hotmail.com>
2024-01-22 15:09:47 +0530

    memory services

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,4 +1,23 @@
 import { Redis } from "@upstash/redis"
 import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 import { Pinecone } from '@pinecone-database/pinecone';
-import { PineconeStore } from "@langchain/vectorstores/pinecone"
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { StringValidation } from "zod";
+
+export type CompanionKey = {
+    companionName: string;
+    modelName: string;
+    userId: string;
+};
+
+export class MemoryManager {
+    private static instance: MemoryManager;
+    private history: Redis;
+    private vectorDBClient: Pinecone;
+
+    public constructor() {
+        this.history = Redis.fromEnv();
+        this.vectorDBClient = new Pinecone();
+    }
+
+}
